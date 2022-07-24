d70388f4d0f907bca313216ee900e4cb8c3fbd9b
josephenoch <enochjoseph01@gmail.com>
2022-07-24 21:42:34 +0100

    :wrench: configure ts sucesfully

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifytodod9bd5be9": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
